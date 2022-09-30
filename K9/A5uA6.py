891bc66686ebe36d6468ecadc32f6284c037e7a5
Jan Schmitt <jan.schmitt@axenton.com>
2022-09-30 11:52:34 +0200

    A6 added

--- K9/A5uA6.py
+++ K9/A5uA6.py
@@ -1,11 +1,14 @@
 def countingStars():
     inputUser = int(input("Stars?"))
     for x in range(1, inputUser+1):
-
         for y in range(1, x+1):
-            # print(x)
             print("*", end='')
+        print()
 
+    while (inputUser > 0):
+        for x in range(1, inputUser):
+            print("*", end='')
+        inputUser -= 1
         print()
 
 
