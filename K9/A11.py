5a07c1c883f6474714c96da1b2aab899b0ef366e
Jan Schmitt <jan.schmitt@axenton.com>
2022-09-30 12:54:27 +0200

    cinema added

--- K9/A11.py
+++ K9/A11.py
@@ -1,1 +1,39 @@
+import random
+
+
 def kino():
+    loge = [["[  ]", "[  ]", "[  ]", "[  ]", "[  ]", "[  ]", "[  ]", "[  ]", "[  ]"],
+            ["[  ]", "[  ]", "[  ]", "[  ]", "[  ]", "[  ]", "[  ]", "[  ]", "[  ]"]]
+
+    room = [["[  ]", "[  ]", "[  ]", "[  ]", "[  ]", "[  ]", "[  ]", "[  ]", "[  ]"],
+            ["[  ]", "[  ]", "[  ]", "[  ]", "[  ]",
+                "[  ]", "[  ]", "[  ]", "[  ]"],
+            ["[  ]", "[  ]", "[  ]", "[  ]", "[  ]",
+                "[  ]", "[  ]", "[  ]", "[  ]"],
+            ["[  ]", "[  ]", "[  ]", "[  ]", "[  ]",
+                "[  ]", "[  ]", "[  ]", "[  ]"],
+            ["[  ]", "[  ]", "[  ]", "[  ]", "[  ]",
+                "[  ]", "[  ]", "[  ]", "[  ]"],
+            ["[  ]", "[  ]", "[  ]", "[  ]", "[  ]", "[  ]", "[  ]", "[  ]", "[  ]"]]
+    setSeats(room)
+    setSeats(loge)
+    printArray(room)
+    printArray(loge)
+
+
+def setSeats(array):
+    numberSeatSeats = int((8*len(array))/3)
+    while (numberSeatSeats > 0):
+        one = random.randint(0, len(array)-1)
+        two = random.randint(0, 8)
+        array[one][two] = "[OO]"
+        numberSeatSeats -= 1
+
+
+def printArray(array):
+    for x in range(len(array)):
+        print(array[x])
+    print()
+
+
+kino()
